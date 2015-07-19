9645eb7c7a41a4229b36898b152830202339e7e3
Nikita <bredikhin.nikita@gmail.com>
2015-07-19 22:36:42 +0400

    tws-spoilers, удалены старые компоненты
    - tws-spoilers поддерживает новые компоненты

--- tws-components_OLD/server/damage_sync.lua
+++
@@ -1,6 +0,0 @@
-addEvent("tws-onClientVehicleDamage", true)
-addEventHandler("tws-onClientVehicleDamage", root,
-	function()
-		triggerClientEvent(root, "tws-updateVehicleComponentsDamage", source)
-	end
-)
--- tws-spoilers/client.lua
+++ tws-spoilers/client.lua
@@ -42,8 +42,8 @@ function setVehicleSpoiler(vehicle, id)
 
 	addVehicleUpgrade(vehicle, spoilersIDs[id])
 	setVehicleSpoilerType(vehicle, "normal")
-
-	exports["tws-components"]:setVehicleTuningComponentLevel(vehicle, "tws-boot", 0)
+	
+	vehicle:setData(vehicle, "tws-boot-level", 0, false)
 end
 
 function setVehicleSpoilerType(vehicle, spoilerType)
@@ -86,5 +86,5 @@ function removeVehicleSpoiler(vehicle)
 		removeVehicleUpgrade(vehicle, id)
 	end
 	
-	exports["tws-components"]:setVehicleTuningComponentLevel(vehicle, "tws-boot", 1)
+	vehicle:setData(vehicle, "tws-boot-level", 1, false)
 end
